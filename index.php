d28cfd45df7d3585ecc97a021243c5c94f4ee5fe
Pawel Urbanczyk <pawel.urbanczyk90@gmail.com>
2017-12-12 13:00:57 +0100

    Dodanie szablonu plików do repozytorium

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="pl">
+	<head>
+		<title>Contact Forn</title>
+		<meta charset="utf-8" />
+		<meta name="description" content="Contact Form"/>
+		<meta name="keywords" content="Contact Form"/>
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1"/>
+		
+	<!--Bootstrap links start-->
+	
+		<!--jQuery-->
+		<script	src="js/jquery.js"></script>
+		
+		<!--Latest compiled and minified CSS-->
+		<link rel="stylesheet" href="css/bootstrap.css">
+		<!--custom css-->
+		<link rel="stylesheet" href="css/style.css">
+		<!--font awesome-->
+		<link rel="stylesheet" href="css/font-awesome.css">
+
+		<!--Latest compiled and minified JavaScript-->
+		<script src="js/bootstrap.js"></script>
+		
+
+	<!--Bootstrap links end-->
+		
+		
+	</head>
+	<body>
+
+	</body>
+</html>
